ee2fe7e0f79468346be33b1857e84d1dac341f73
xenotime <sandeepkhoj@gmail.com>
2022-06-15 13:27:59 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: `resume`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "jmesob5c",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
